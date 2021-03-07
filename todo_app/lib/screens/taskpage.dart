e38da1fedc6ee118d4c0eaa89b91a138b6e4e68b
Hakimi Ridzuan <mrhakimi88@gmail.com>
2021-03-07 23:28:49 +0800

    Task Description

--- todo_app/lib/screens/taskpage.dart
+++ todo_app/lib/screens/taskpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets.dart';
+
 class TaskPage extends StatefulWidget {
   @override
   _TaskPageState createState() => _TaskPageState();
@@ -9,10 +11,60 @@ class _TaskPageState extends State<TaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Task Page"),
-        ),
+      body: SafeArea(
+        child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 24, bottom: 6),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Image(
+                        image: AssetImage("assets/images/back_arrow_icon.png"),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Title", border: InputBorder.none),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF211551),
+                    ),
+                  ))
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 12),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter description",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 24
+                  )
+                ),
+              ),
+            ),
+            TodoWidget(
+              text: "First task",
+              isDone: false,
+            ),
+            TodoWidget(
+              isDone: true,
+            )
+          ],
+        )),
       ),
     );
   }
